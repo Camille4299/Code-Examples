79087b5c87ff91bce45314715ceb929d00b77fcd
Camille4299 <105239425+Camille4299@users.noreply.github.com>
2022-06-13 18:45:16 +0100

    Delete Security directory

--- Security/sessions.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-
-    if((isset($_SESSION['message'])) && ($_SESSION['message'] !== "Authenticated"))
-    {
-        header("Location: index.php?error=wrongDetails");
-    }
-?>
